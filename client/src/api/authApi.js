8a87df7940b04b5939a0018e1c52e704f261d5a8
Steak-bit <78823085+Steak-bit@users.noreply.github.com>
2023-11-11 12:24:45 +0100

    ready for deployment

--- client/src/api/authApi.js
+++ client/src/api/authApi.js
@@ -4,127 +4,127 @@ axios.defaults.withCredentials = true
 
 // customer
 export async function onRegistrationCustomer(registrationDataCustomer) {
-    return await axios.post('http://localhost:8000/api/register-customers', registrationDataCustomer)
+    return await axios.post('/register-customers', registrationDataCustomer)
 }
 
 export async function onLoginCustomer(LoginDataCustomer) {
-    return await axios.post('http://localhost:8000/api/login-customers', LoginDataCustomer)
+    return await axios.post('/login-customers', LoginDataCustomer)
 }
 
 export async function fetchProtectedCustomer() {
-    return await axios.get('http://localhost:8000/api/protected-customers')
+    return await axios.get('/protected-customers')
 }
 
 export async function fetchProducts() {
-    return await axios.get('http://localhost:8000/api/products')
+    return await axios.get('/products')
 }
 
 export async function fetchProductsCustomer() {
-    return await axios.get('http://localhost:8000/api/products-customer')
+    return await axios.get('/products-customer')
 }
 
 export async function reserveProductsCustomer(name, description, price, material) {
-    return await axios.post(`http://localhost:8000/api/reserve-products/${name}/${description}/${price}/${material}`)
+    return await axios.post(`/reserve-products/${name}/${description}/${price}/${material}`)
 }
 
 export async function removeProduct(Id) {
-    return await axios.delete(`http://localhost:8000/api/remove-product/${Id}`)
+    return await axios.delete(`/remove-product/${Id}`)
 }
 
 export async function fetchTravels() {
-    return await axios.get('http://localhost:8000/api/travels')
+    return await axios.get('/travels')
 }
 
 export async function fetchTravelsCustomer() {
-    return await axios.get('http://localhost:8000/api/travels-customer')
+    return await axios.get('/travels-customer')
 }
 
 export async function reserveTravelsCustomer(name, departure, destination, date, price, length) {
-    return await axios.post(`http://localhost:8000/api/reserve-travels/${name}/${departure}/${destination}/${date}/${price}/${length}`)
+    return await axios.post(`/reserve-travels/${name}/${departure}/${destination}/${date}/${price}/${length}`)
 }
 
 export async function removeTravel(Id) {
-    return await axios.delete(`http://localhost:8000/api/remove-travel/${Id}`)
+    return await axios.delete(`remove-travel/${Id}`)
 }
 
 
 // employee
 export async function onLoginEmployee(LoginDataEmployee) {
-    return await axios.post('http://localhost:8000/api/login-employees', LoginDataEmployee)
+    return await axios.post('login-employees', LoginDataEmployee)
 }
 
 export async function fetchProtectedEmployee() {
-    return await axios.get('http://localhost:8000/api/protected-employees')
+    return await axios.get('/protected-employees')
 }
 
 export async function fetchProjectsEmployee() {
-    return await axios.get('http://localhost:8000/api/projects-employee')
+    return await axios.get('/projects-employee')
 }
 
 export async function editProgressEmployee(ProgressEmployee) {
-    return await axios.put('http://localhost:8000/api/progress-employee', ProgressEmployee);
+    return await axios.put('/progress-employee', ProgressEmployee);
 }
   
 
 // management
 export async function onLoginManager(LoginDataManager) {
-    return await axios.post('http://localhost:8000/api/login-managers', LoginDataManager)
+    return await axios.post('/login-managers', LoginDataManager)
 }
 
 export async function fetchProtectedManager() {
-    return await axios.get('http://localhost:8000/api/protected-managers')
+    return await axios.get('/protected-managers')
 }
 
 export async function fetchEmployeestoManagement() {
-    return await axios.get('http://localhost:8000/api/get-employees')
+    return await axios.get('/get-employees')
 }
 
 export async function promoteEmployee(PromoteEmployee) {
-    return await axios.put('http://localhost:8000/api/promote-employee', PromoteEmployee)
+    return await axios.put('/promote-employee', PromoteEmployee)
 }
 
 export async function fireEmployee(employeeId) {
-    return await axios.delete(`http://localhost:8000/api/fire-employee/${employeeId}`)
+    return await axios.delete(`/fire-employee/${employeeId}`)
 }
 
 export async function fetchTools() {
-    return await axios.get('http://localhost:8000/api/tools')
+    return await axios.get('/tools')
 }
 
 export async function fetchEquipments() {
-    return await axios.get('http://localhost:8000/api/equipments')
+    return await axios.get('/equipments')
 }
 
 export async function fetchVehicles() {
-    return await axios.get('http://localhost:8000/api/vehicles')
+    return await axios.get('/vehicles')
 }
 
 export async function editAmortisation(EditAmortisation) {
-    return await axios.put('http://localhost:8000/api/edit-amortisation', EditAmortisation)
+    return await axios.put('/edit-amortisation', EditAmortisation)
 }
 
 export async function deleteProperties(Id) {
-    return await axios.delete(`http://localhost:8000/api/delete-properties/${Id}`)
+    return await axios.delete(`/delete-properties/${Id}`)
 }
 
 export async function fetchProjectstoManagement() {
-    return await axios.get('http://localhost:8000/api/get-projects')
+    return await axios.get('/get-projects')
 }
 
 export async function createProjectstoEmployee(CreateEmployeeProjects) {
-    return await axios.post('http://localhost:8000/api/create-projects', CreateEmployeeProjects)
+    return await axios.post('/create-projects', CreateEmployeeProjects)
 }
 
 export async function editProjects(EditProjects) {
-    return await axios.put('http://localhost:8000/api/edit-projects', EditProjects)
+    return await axios.put('/edit-projects', EditProjects)
 }
 
 export async function deleteProjectsMan(projectId) {
-    return await axios.delete(`http://localhost:8000/api/delete-projects/${projectId}`)
+    return await axios.delete(`/delete-projects/${projectId}`)
 }
 
 
 // all
 export async function onLogout() {
-    return await axios.get('http://localhost:8000/api/logout')
+    return await axios.get('/logout')
 }
--- src/index.js
+++ src/index.js
@@ -3,7 +3,9 @@ const app = express()
 const { PORT, CLIENT_URL } = require('./constants')
 const cookieParser = require('cookie-parser')
 const passport = require('passport')
+const path = require("path")
 const cors = require('cors')
+
 
 //import passport middleware
 require('./middlewares/passport-middleware')
@@ -14,17 +16,27 @@ app.use(cookieParser())
 app.use(cors({ origin: CLIENT_URL, credentials: true }))
 app.use(passport.initialize())
 
+if(process.env.NODE_ENV === "production") {
+    // server static content
+    // npm run build
+    app.use(express.static(path.join(__dirname, "client/build")))
+}
+
 // impoort routes
 const authRoutes = require('./routes/auth')
 
 //initialize routes
-app.use('/api', authRoutes)
+app.use( authRoutes)
+
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "client/build/index.html"))
+})
 
 //app start
 const appStart = () => {
     try {
         app.listen(PORT, () => {
-            console.log(`App is running at http://localhost:${PORT}`)
+            console.log(`App is running on port ${PORT}`)
         })
     } catch (error) {
         console.log(`Error: ${error.message}`)
