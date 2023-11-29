459ed05c08c29e9f22f6b3522f6c1495921f2722
Steak-bit <78823085+Steak-bit@users.noreply.github.com>
2023-11-29 23:38:27 +0100

    databse config

--- src/db/index.js
+++ src/db/index.js
@@ -1,17 +1,13 @@
 const Pool = require('pg').Pool
 require("dotenv").config()
 
-const devConfig = 
-    `postgresql://
-    ${process.env.PG_USER}
-    :${process.env.PG_PASSWORD}
-    @${process.env.PG_HOST}
-    :${process.env.PG_PORT}
-    /${process.env.PG_DATABASE}`;
-
 const pool = new Pool({
-    devConfig
-  });
+    user: process.env.PG_USER,
+    host: process.env.PG_HOST,
+    database: process.env.PG_DATABASE,
+    password: process.env.PG_PASSWORD,
+    port: process.env.PG_PORT
+  })
 
 module.exports = {
     query: (text, params) => pool.query(text, params),
